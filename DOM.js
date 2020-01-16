16c1dc5f9271d525df2c5a153a3dd651a611e54e
xbranw <bwoolley@roc-dev.com>
2020-01-16 11:53:37 +0100

    Opdracht 2.1

---
+++ DOM.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Dit is de pagina titel";
+document.getElementById("subkop").innerText = "Dit is de pagina subkop";
+document.getElementById("auto1").src = "img/auto1.jpg";
+document.getElementById("auto2").src = "img/auto2.jpg";
