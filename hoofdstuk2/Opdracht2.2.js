efbf37ca4e2346fb593ef5647a5a5854ffbdc21a
xbranw <bwoolley@roc-dev.com>
2020-01-30 12:10:40 +0100

    Hoofdstuk 2

--- hoofdstuk2/Opdracht2.2.js
+++ hoofdstuk2/Opdracht2.2.js
@@ -1,1 +1,27 @@
-document.getElementById("bl1").innerHTML
+var bl1 = document.getElementById("bl1");
+bl1.style.backgroundColor = "aqua";
+bl1.style.width = "100px";
+bl1.style.height = "100px";
+bl1.style.textAlign = "center";
+
+
+var bl2 = document.getElementById("bl2");
+bl2.style.backgroundColor = "green";
+bl2.style.width = "100px";
+bl2.style.height = "100px";
+bl2.style.marginTop = "20px";
+bl2.style.textAlign = "center";
+
+
+var bl3 = document.getElementById("bl3");
+bl3.style.backgroundColor = "yellow";
+bl3.style.width = "100px";
+bl3.style.height = "100px";
+bl3.style.marginTop = "40px";
+bl3.style.textAlign = "right";
+
+var bl4 = document.getElementById("bl4");
+bl4.style.backgroundColor = "red";
+bl4.style.width = "100px";
+bl4.style.height = "100px";
+
